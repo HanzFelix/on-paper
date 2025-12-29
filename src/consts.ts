0a9e21f9f387fdd63fac328787f784e723960c6e
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-12-29 21:33:20 +0800

    feat: initialize project

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "On Paper";
+export const SITE_DESCRIPTION =
+  "Documenting my dev journey in a paper-friendly format.";
+export const SITE_TITLE_LOWER = "on-paper";
