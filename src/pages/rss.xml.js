0bca47381174172cc7b3ba55d1307d6d7927df99
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2026-01-09 11:52:39 +0800

    feat: update relative URL

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/that-discussed/${post.id}/`,
+      link: `/keen-on/${post.id}/`,
     })),
   });
 }
